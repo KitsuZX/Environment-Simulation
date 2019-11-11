39f3aafca185aebab4d02ea26a4f67a84d3630f6
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-11 16:55:36 +0100

    Se instancian los animales

--- Environment Simulation/Assets/Scripts/Ecosystem/EcosystemGenerator.cs
+++ Environment Simulation/Assets/Scripts/Ecosystem/EcosystemGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine;
 
+#pragma warning disable 649
 [RequireComponent(typeof(TerrainGenerator))]
 public class EcosystemGenerator : MonoBehaviour
 {    
@@ -15,7 +16,13 @@ public class EcosystemGenerator : MonoBehaviour
     [Range(0, 1)]
     [SerializeField] private float bushThreshold = 0.05f;
 
+    [Header("Animals")]
+    [SerializeField] private int rabbitCount = 10;
+    [SerializeField] private int foxCount = 10;
+
     [Header("References")]
+    [SerializeField] private GameObject rabbitPrefab;
+    [SerializeField] private GameObject foxPrefab;
     [SerializeField] private GameObject treePrefab;
     [SerializeField] private Transform treesParent;
     [SerializeField] private GameObject bushPrefab;
@@ -31,6 +38,7 @@ public class EcosystemGenerator : MonoBehaviour
 
         GenerateTrees();
         GenerateBushes();
+        GenerateAnimals();
     }
 
     private void GenerateTrees()
@@ -77,6 +85,37 @@ public class EcosystemGenerator : MonoBehaviour
                     }
                 }
             }
+        }
+    }
+
+    private void GenerateAnimals()
+    {
+        int x;
+        int y;
+
+        Vector3 offset = new Vector3(0, 0.5f, 0);
+        for (int i = 0; i < rabbitCount; i++)
+        {
+            do
+            {
+                x = Random.Range(0, terrainData.size);
+                y = Random.Range(0, terrainData.size);
+            } while (!terrainData.walkable[x, y]);
+
+
+            Instantiate(rabbitPrefab, terrainData.tileCentres[x, y] + offset, Quaternion.identity);
+        }
+
+        for (int i = 0; i < foxCount; i++)
+        {
+            do
+            {
+                x = Random.Range(0, terrainData.size);
+                y = Random.Range(0, terrainData.size);
+            } while (!terrainData.walkable[x, y]);
+
+
+            Instantiate(foxPrefab, terrainData.tileCentres[x, y] + offset, Quaternion.identity);
         }
     }
 }
