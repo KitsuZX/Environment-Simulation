f00e77c032e472b2cb917c38bc7a41c5f8b8a0cc
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-11 16:10:24 +0100

    Necesidades básicas

---
+++ Environment Simulation/Assets/Scripts/AnimalNeeds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Genes))]
+public class AnimalNeeds : MonoBehaviour
+{
+    private Genes genes;
+
+    [SerializeField, Range(0, 1)] private float energyDecayPerSecond = 0.1f;
+    [SerializeField, Range(0, 1)] private float hidrationDecayPerSecond = 0.1f;
+
+    public float CurrentAge { get; private set; }
+    public float CurrentEnergy { get; private set; }
+    public float CurrentHidration { get; private set; }
+
+    private void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+
+        CurrentAge += dt / 60;
+        CurrentEnergy = Mathf.Max(CurrentEnergy - energyDecayPerSecond * dt, 0);
+        CurrentHidration = Mathf.Max(CurrentHidration - hidrationDecayPerSecond * dt, 0);
+    }
+
+    private void Awake()
+    {
+        genes = GetComponent<Genes>();
+
+        CurrentAge = 0;
+        CurrentEnergy = genes.maxEnergy;
+        CurrentHidration = genes.maxHidration;
+    }
+}
---
+++ Environment Simulation/Assets/Scripts/Genes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genes : MonoBehaviour
+{
+    public float maxEnergy;
+    public float maxHidration;
+    public float hopDistance;
+    public float childCountMean;
+    public float perceptionRadius;
+    public float gestationPeriodLength;
+    public Vector2 reproductiveAgeRange;
+    public float sexAppeal;
+}
