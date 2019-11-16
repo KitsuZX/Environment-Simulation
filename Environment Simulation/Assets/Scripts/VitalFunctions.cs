2ba407460166019f4b63864cd78e581c57eb1eb8
DDChetok <43351928+DDChetok@users.noreply.github.com>
2019-11-16 15:33:45 +0100

    no se

---
+++ Environment Simulation/Assets/Scripts/VitalFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalFunctions : MonoBehaviour
+{
+
+    private float CurrentAge;
+    private float CurrentEnergy;
+    private float CurrentHydration;
+
+      
+    public void EatFood()
+    {
+
+    }
+
+}
