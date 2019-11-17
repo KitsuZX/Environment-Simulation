0e9478896386aa14de40354335ae85539682c544
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-17 17:21:22 +0100

    TimeAccelerator

---
+++ Environment Simulation/Assets/TimeAccelerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAccelerator : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 5)] private float timeScale = 1;
+
+    private void FixedUpdate()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+}
