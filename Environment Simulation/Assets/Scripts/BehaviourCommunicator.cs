07d17990ba38c6418a58a1cadd38a910b2e82062
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-18 10:35:50 +0100

    Bocadillos sólo hasta cierta distancia. Bocadillos con un tamaño en espacio de pantalla mínimo.

--- Environment Simulation/Assets/Scripts/BehaviourCommunicator.cs
+++ Environment Simulation/Assets/Scripts/BehaviourCommunicator.cs
@@ -5,25 +5,63 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class BehaviourCommunicator : MonoBehaviour
 {
+    [SerializeField] private float maxShowDistance = 10;
+
     private new Transform camera;
     private SpriteRenderer spriteRenderer;
+
+    private bool isHidden;
+
 
     public void SetSprite(Sprite sprite)
     {
         spriteRenderer.sprite = sprite;
 
-        spriteRenderer.enabled = sprite;
+        spriteRenderer.enabled = !isHidden && sprite;
     }
 
 
     private void Update()
     {
-        transform.LookAt(camera, camera.up);
+        float distance = Vector3.Distance(transform.position, camera.position);
+
+        if (isHidden && distance < maxShowDistance) Show();
+        else if (!isHidden && distance > maxShowDistance) Hide();
+
+        if (!isHidden) transform.LookAt(camera, camera.up);
     }
+
+    private void Show()
+    {
+        foreach (Transform transform in transform)
+        {
+            transform.gameObject.SetActive(true);
+        }
+
+        spriteRenderer.enabled = true;
+        isHidden = false;
+    }
+
+    private void Hide()
+    {
+        foreach (Transform transform in transform)
+        {
+            transform.gameObject.SetActive(false);
+        }
+
+        spriteRenderer.enabled = false;
+        isHidden = true;
+    }
+
 
     private void Awake()
     {
         camera = Camera.main.transform;
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Hide();
     }
 }
---
+++ Environment Simulation/Assets/Scripts/MinimumScreenSpaceSize.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimumScreenSpaceSize : MonoBehaviour
+{
+    public float minimumScale = 0.1f;
+
+    private new Camera camera;
+    private float trueScale;
+
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(camera.transform.position, transform.position);
+        float frustumHeight = 2.0f * distance * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float realScreenSpaceSize = trueScale / frustumHeight;
+
+        if (realScreenSpaceSize < minimumScale) transform.localScale = Vector3.one * minimumScale * frustumHeight;
+        else transform.localScale = Vector3.one * trueScale;
+    }
+
+    private void Start()
+    {
+        camera = Camera.main;
+        trueScale = transform.localScale.x;
+    }
+}
